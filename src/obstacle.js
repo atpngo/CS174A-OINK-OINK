1582db819940eaaff5ed1b75870df2cb0188d366
Andy Ngo <andytpngo@g.ucla.edu>
2023-06-09 02:37:56 -0700

    fixed obstacles

--- src/obstacle.js
+++ src/obstacle.js
@@ -1,4 +1,5 @@
 import {defs, tiny} from './examples/common.js';
+import { Shape_From_File } from './examples/obj-file-demo.js';
 
 const {
     Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene, Texture,
@@ -11,35 +12,52 @@ const getRandomNumber = max => {
 }
 
 // set the lane positions
-const DODGE_AMPLITUDE = 2.6;
-const JUMP_AMPLITUDE = 5;
-const DODGE_LANE_POSITIONS = [-DODGE_AMPLITUDE, 0, DODGE_AMPLITUDE];
-const JUMP_LANE_POSITIONS = [-JUMP_AMPLITUDE, 0, JUMP_AMPLITUDE];
+const AMPLITUDE = 20;
+const LANE_POSITIONS = [-AMPLITUDE, 0, AMPLITUDE];
 const TYPES = ["jump", "dodge", "duck"];
 const NUM_TYPES = TYPES.length;
+
+const getSpeed = (type, multiplier) => {
+    if (type === "jump")
+    {
+        return Mat4.translation(-0.225*multiplier, 0, 0);
+    }
+    if (type === "dodge")
+    {
+        return Mat4.translation(0, 0, 0.3*multiplier);
+    }
+    if (type === "duck")
+    {
+        return Mat4.translation(0, 0, 0.3*multiplier);
+    }
+}
+
 const BASE_SPEEDS = {
-    jump: 0.45,
-    dodge: 0.3,
-    duck: 0.4,
+    jump: Mat4.translation(-0.225, 0, 0),
+    dodge: Mat4.translation(0, 0, 0.3),
+    duck: Mat4.translation(0, 0, 0.3),
 }
 const OFFSETS = {
-    jump: 40,
-    dodge: 26.675,
-    duck: 40
+    jump: 200,
+    dodge: 200,
+    duck: 200
 }
 export class Obstacle
 {
-    constructor(lane, type, offset)
+    constructor(lane, type, offset, speed_multiplier)
     {
         this.position = Mat4.identity();
+        this.avoided = false;
+        this.offset = offset;
+        this.speed_multiplier = speed_multiplier;
         // get random lane position
         this.lane = lane;
         this.type_id = type;
         this.type = TYPES[this.type_id];
         this.shapes = {
-            jump: new defs.Tetrahedron(0),
+            jump: new Shape_From_File("assets/Spikes.obj"),
             dodge: new Cube(),
-            duck: new Cube()
+            duck: new Shape_From_File("assets/bird.obj")
         };
         this.materials = {
             jump: new Material(new defs.Phong_Shader(), {ambient: 0.4, diffusivity: 0.6, color: hex_color("#b0b0b0")}),
@@ -49,20 +67,48 @@ export class Obstacle
                 ambient: 1.0, diffusivity: 0.1, specularity: 0.1,
                 texture: new Texture("assets/images/brick.jpeg")
             }),
-            duck: new Material(new defs.Phong_Shader(), {ambient: 0.4, diffusivity: 0.6, color: hex_color("#e28ee8")}),
+            duck: new Material(new Textured_Phong(), {
+                color: hex_color("#000000"),
+                ambient: 1.0, diffusivity: 0.1, specularity: 0.1,
+                texture: new Texture("assets/images/bird.jpeg")
+            }),
+            // duck: new Material(new defs.Phong_Shader(), {ambient: 0.4, diffusivity: 1, color: hex_color("#eb3d3d")}),
         }
         this.initialPositions = {
-            jump: Mat4.identity().times(Mat4.scale(4, 5, 2))
-                                 .times(Mat4.translation(JUMP_LANE_POSITIONS[this.lane], -0.8, -(offset+OFFSETS.jump))),
-            dodge: Mat4.identity().times(Mat4.scale(6, 10, 3))
-                                  .times(Mat4.translation(DODGE_LANE_POSITIONS[this.lane], 0.6, -(offset+OFFSETS.dodge))),
-            duck: Mat4.identity().times(Mat4.scale(6, 2, 2))
-                                  .times(Mat4.translation(DODGE_LANE_POSITIONS[this.lane], 4, -(offset+OFFSETS.duck))),
+            jump: Mat4.identity().times(Mat4.translation(LANE_POSITIONS[this.lane], -2, -(offset+OFFSETS.jump)))
+                                 .times(Mat4.rotation(Math.PI/2, 0, 1, 0,))
+                                 .times(Mat4.rotation(-Math.PI/2, 1, 0, 0))
+                                 .times(Mat4.scale(4, 4, 4)),
+            dodge: Mat4.identity().times(Mat4.translation(LANE_POSITIONS[this.lane], 5, -(offset+OFFSETS.dodge)))
+                                  .times(Mat4.scale(7, 10, 3)),
+            duck: Mat4.identity().times(Mat4.translation(LANE_POSITIONS[this.lane], 6.5, -(offset+OFFSETS.duck)))
+                                 .times(Mat4.scale(3, 3, 3))
 
         }
         this.shape = this.shapes[this.type];
         this.position = this.initialPositions[this.type];
         this.material = this.materials[this.type]
+    }
+
+    calculateNewPosition(type, lane)
+    {
+        if (type === "jump")
+        {
+            return Mat4.identity().times(Mat4.translation(LANE_POSITIONS[lane], -2, -(this.offset+OFFSETS.jump)))
+                                  .times(Mat4.rotation(Math.PI/2, 0, 1, 0,))
+                                  .times(Mat4.rotation(-Math.PI/2, 1, 0, 0))
+                                  .times(Mat4.scale(4, 4, 4));
+        }
+        else if (type === "dodge")
+        {
+            return Mat4.identity().times(Mat4.translation(LANE_POSITIONS[lane], 5, -(this.offset+OFFSETS.dodge)))
+                                  .times(Mat4.scale(7, 10, 3));
+        }
+        else if (type === "duck")
+        {
+            return Mat4.identity().times(Mat4.translation(LANE_POSITIONS[lane], 6.5, -(this.offset+OFFSETS.duck)))
+                                  .times(Mat4.scale(3, 3, 3));
+        }
     }
 
     setLane(lane)
@@ -95,7 +141,7 @@ export class Obstacle
 
     resetPosition()
     {
-        this.position = this.initialPositions[this.type];
+        this.position = this.calculateNewPosition(this.type, this.lane);
     }
 
     getPosition()
@@ -103,10 +149,10 @@ export class Obstacle
         return this.position;
     }
 
-    render(context, program_state, t, dt)
+    render(context, program_state, multiplier)
     {
         // move objects toward scene
-        this.position = this.position.times(Mat4.translation(0, 0, BASE_SPEEDS[this.type]))
+        this.position = this.position.times(getSpeed(this.type, multiplier));
         this.shape.draw(context, program_state, this.position, this.material);
     }
 }
--- src/project.js
+++ src/project.js
@@ -14,6 +14,20 @@ const getRandomNumber = max => {
     return Math.floor(Math.random()*max)
 }
 
+const getNewPositions = number => {
+    // return an array of random numbers between 0 and 3 
+    const first = getRandomNumber(3);
+    let second = getRandomNumber(3);
+    while (second === first)
+    {
+        second = getRandomNumber(3);
+    }
+    let ans = [];
+    ans.push(first);
+    ans.push(second);
+    return ans;
+}
+
 export class Project extends Scene {
     /**
      *  **Base_scene** is a Scene that can be added to any display canvas.
@@ -23,10 +37,13 @@ export class Project extends Scene {
         // constructor(): Scenes begin by populating initial values like the Shapes and Materials they'll need.
         super();
         this.END_OF_SCREEN = 40;
+        this.PIG_FRONT = 12;
+        this.PIG_BACK = 26;
+        this.speed_multiplier = 1;
         this.pig = new Pig();
         this.obstacles = [];
-        this.OBSTACLE_OFFSET = 20;
-        this.NUM_WAVES = 3;
+        this.OBSTACLE_OFFSET = 40;
+        this.NUM_WAVES = 4;
         this.NUM_OBSTACLES = this.NUM_WAVES*2;
         this.score_count = 0;
         this.total_score = 0;
@@ -36,6 +53,9 @@ export class Project extends Scene {
         this.background_sound_flag = false;
         this.blinking_flag = false;
         this.blinking_count = 0;
+        // to make the ground increase in speed we might have to ditch the texture scrolling and add ground objects that spawn/despawn
+        this.SPEED_INCREMENT = 0.05;
+        this.SPEED_INCREMENT = 0.05;
 
         this.background_sound = new Audio("assets/audio/background.mp3");
         this.pig_sound = new Audio("assets/audio/pig-grunt.mp3");
@@ -58,24 +78,21 @@ export class Project extends Scene {
             spike: new defs.Tetrahedron(0),
             text: new Text_Line(35),
         }
-        // this.shapes = {
-        //     box_1: new Cube(),
-        //     box_2: new Cube(),
-        //     axis: new Axis_Arrows(),
-        //     // pig: new Shape_From_File("assets/testpig.obj"),
-        //     text: new Text_Line(35),
-
-        // }
 
         const textured = new defs.Textured_Phong(1);
         this.materials = {
             obstacle: new Material(new defs.Phong_Shader(), {ambient: 0.4, diffusivity: 0.6, color: hex_color("#ff0000")}),
-            sky: new Material(new Textured_Phong(), {
+            sky: new Material(new Texture_Scroll_Y(), {
                 color: hex_color("#000000"),
                 ambient: 1.0, diffusivity: 0.1, specularity: 0.1,
-                texture: new Texture("assets/images/cloudy.jpeg", "NEAREST")
+                texture: new Texture("assets/images/sky.jpg", "NEAREST")
             }),
-            ground: new Material(new Textured_Phong(), {
+            ground: new Material(new Texture_Scroll_X(), {
+                color: hex_color("#000000"),
+                ambient: 1.0, diffusivity: 0.1, specularity: 0.1,
+                texture: new Texture("assets/images/grass.jpeg", "NEAREST")
+            }),
+            ground_loading: new Material(new Textured_Phong(), {
                 color: hex_color("#000000"),
                 ambient: 1.0, diffusivity: 0.1, specularity: 0.1,
                 texture: new Texture("assets/images/grass.jpeg", "NEAREST")
@@ -91,13 +108,15 @@ export class Project extends Scene {
         .times(Mat4.translation(0, -5, -5))
         // width, thickness, length
         .times(Mat4.scale(60, 1, 125))
-        .times(Mat4.translation(0, 0, -0.5))
+        .times(Mat4.translation(0, 0, -0.5));
+
     }
 
     make_control_panel() {
-        // this.key_triggered_button("Console Log", ["c"], () => {
-        //         console.log(this.obstacles);
-        // })
+        this.key_triggered_button("Console Log", ["c"], () => {
+                console.log(this.pig.duck);
+                console.log(this.pig.ducking);
+        })
         this.key_triggered_button("Start", ['Enter'], () => {
             if (!this.game_start && !this.game_over) {
             this.game_start = true;
@@ -477,10 +496,33 @@ export class Project extends Scene {
 
     }
 
+    check_collision(pig, obstacle)
+    {
+        // Check if obstacle is in the same lane as the pig
+        if ((pig.left_pos && obstacle.lane === 0) || (pig.right_pos && obstacle.lane === 2) || (!pig.right_pos && !pig.left_pos && obstacle.lane === 1))
+        {
+             // Check if obstacle is close to the pig
+            if (obstacle.position[2][3] >= this.PIG_FRONT && obstacle.position[2][3] <= this.PIG_BACK && !obstacle.avoided)
+            {
+                // Check pig status and obstacle type
+                if ((pig.jump && obstacle.type === "jump") || (pig.duck && obstacle.type === "duck"))
+                {
+                    obstacle.avoided = true;
+                    return false;
+                }
+
+                return true;
+            }
+            
+            return false;
+        }
+        return false;;
+    }
+
 
     display(context, program_state) {
         if (!context.scratchpad.controls) {
-            // this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
+            this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
             program_state.set_camera(
                 Mat4.identity()
                 .times(Mat4.translation(0, -2, -58))
@@ -511,17 +553,23 @@ export class Project extends Scene {
                                          .times(Mat4.translation(0, 0, -1));
         // this.shapes.box_1.draw(context, program_state, model_transform, this.materials.obstacle);
         // this.shapes.spike.draw(context, program_state, model_transform, this.materials.obstacle);
-        this.shapes.ground.draw(context, program_state, this.ground_transform, this.materials.ground)
         this.shapes.sphere_3.draw(context, program_state, Mat4.scale(100, 100, 200), this.materials.sky);
 
-        // draw obstacles
-
+        // increment speed multiplier
         if (!this.game_over && this.game_start) {
+
             for (let i=this.NUM_OBSTACLES-1; i>=0; i--)
             {
+                
                 if (this.obstacles[i].position[2][3] < this.END_OF_SCREEN)
                 {
-                    this.obstacles[i].render(context, program_state, t, dt);
+                    this.obstacles[i].render(context, program_state, this.speed_multiplier+Math.floor(this.total_score/100)*this.SPEED_INCREMENT);
+                    // check for collisions
+                    if (this.check_collision(this.pig, this.obstacles[i]))
+                    {
+                        console.log("COLLISION");
+                        this.game_over = true;
+                    };
                 }
                 else
                 {
@@ -530,181 +578,39 @@ export class Project extends Scene {
                     this.obstacles[i].setTypeID(getRandomNumber(3));
                     this.obstacles[i].setLane(getRandomNumber(3));
                     this.obstacles[i].resetPosition();
+                    this.obstacles[i].avoided = false;
                 }
             }
-
-
-
-
-            // else {
-    
             
             this.draw_pig(context,program_state,model_transform);
             this.draw_score(context,program_state,model_transform);
+            this.shapes.ground.draw(context, program_state, this.ground_transform, this.materials.ground)
+
         }
         else if (this.game_over) {
             this.draw_game_over(context,program_state,model_transform);
             this.draw_score(context,program_state,model_transform);
+            this.speed_multiplier = 1;
+            // reset obstacles
+            this.obstacles = [];
+            for (let i=0; i<this.NUM_WAVES; i++)
+            {
+                // calculate offset
+                this.obstacles.push(new Obstacle(getRandomNumber(3), getRandomNumber(3), i*this.OBSTACLE_OFFSET));
+                this.obstacles.push(new Obstacle(getRandomNumber(3), getRandomNumber(3), i*this.OBSTACLE_OFFSET));
+            }
         }
         else {
             this.draw_start(context,program_state,model_transform);
             this.draw_loading_pig(context,program_state,model_transform);
+            this.shapes.ground.draw(context, program_state, this.ground_transform, this.materials.ground_loading)
+
         }
-    }
-}
-
-
-
-class Gouraud_Shader extends Shader {
-    // This is a Shader using Phong_Shader as template
-    // TODO: Modify the glsl coder here to create a Gouraud Shader (Planet 2)
-
-    constructor(num_lights = 2) {
-        super();
-        this.num_lights = num_lights;
-    }
-
-    shared_glsl_code() {
-        // ********* SHARED CODE, INCLUDED IN BOTH SHADERS *********
-        return ` 
-        precision mediump float;
-        const int N_LIGHTS = ` + this.num_lights + `;
-        uniform float ambient, diffusivity, specularity, smoothness;
-        uniform vec4 light_positions_or_vectors[N_LIGHTS], light_colors[N_LIGHTS];
-        uniform float light_attenuation_factors[N_LIGHTS];
-        uniform vec4 shape_color;
-        uniform vec3 squared_scale, camera_center;
-
-        // Specifier "varying" means a variable's final value will be passed from the vertex shader
-        // on to the next phase (fragment shader), then interpolated per-fragment, weighted by the
-        // pixel fragment's proximity to each of the 3 vertices (barycentric interpolation).
-        varying vec3 N, vertex_worldspace;
-        varying vec4 vertex_color;
-        // ***** PHONG SHADING HAPPENS HERE: *****                                       
-        vec3 phong_model_lights( vec3 N, vec3 vertex_worldspace ){                                        
-            // phong_model_lights():  Add up the lights' contributions.
-            vec3 E = normalize( camera_center - vertex_worldspace );
-            vec3 result = vec3( 0.0 );
-            for(int i = 0; i < N_LIGHTS; i++){
-                // Lights store homogeneous coords - either a position or vector.  If w is 0, the 
-                // light will appear directional (uniform direction from all points), and we 
-                // simply obtain a vector towards the light by directly using the stored value.
-                // Otherwise if w is 1 it will appear as a point light -- compute the vector to 
-                // the point light's location from the current surface point.  In either case, 
-                // fade (attenuate) the light as the vector needed to reach it gets longer.  
-                vec3 surface_to_light_vector = light_positions_or_vectors[i].xyz - 
-                                               light_positions_or_vectors[i].w * vertex_worldspace;                                             
-                float distance_to_light = length( surface_to_light_vector );
-
-                vec3 L = normalize( surface_to_light_vector );
-                vec3 H = normalize( L + E );
-                // Compute the diffuse and specular components from the Phong
-                // Reflection Model, using Blinn's "halfway vector" method:
-                float diffuse  =      max( dot( N, L ), 0.0 );
-                float specular = pow( max( dot( N, H ), 0.0 ), smoothness );
-                float attenuation = 1.0 / (1.0 + light_attenuation_factors[i] * distance_to_light * distance_to_light );
-                
-                vec3 light_contribution = shape_color.xyz * light_colors[i].xyz * diffusivity * diffuse
-                                                          + light_colors[i].xyz * specularity * specular;
-                result += attenuation * light_contribution;
-            }
-            return result;
-        } `;
-    }
-
-    vertex_glsl_code() {
-        // ********* VERTEX SHADER *********
-        return this.shared_glsl_code() + `
-            attribute vec3 position, normal;                            
-            // Position is expressed in object coordinates.
-            
-            uniform mat4 model_transform;
-            uniform mat4 projection_camera_model_transform;
-    
-            void main(){                                                                   
-                // The vertex's final resting place (in NDCS):
-                gl_Position = projection_camera_model_transform * vec4( position, 1.0 );
-                // The final normal vector in screen space.
-                N = normalize( mat3( model_transform ) * normal / squared_scale);
-                vertex_worldspace = ( model_transform * vec4( position, 1.0 ) ).xyz;
-                vertex_color = vec4(shape_color.xyz * ambient, shape_color.w);
-                vertex_color.xyz += phong_model_lights(N, vertex_worldspace);
-            } `;
-    }
-
-    fragment_glsl_code() {
-        // ********* FRAGMENT SHADER *********
-        // A fragment is a pixel that's overlapped by the current triangle.
-        // Fragments affect the final image or get discarded due to depth.
-        return this.shared_glsl_code() + `
-            void main(){                                                           
-                gl_FragColor = vertex_color;
-                return;
-            } `;
-    }
-
-    send_material(gl, gpu, material) {
-        // send_material(): Send the desired shape-wide material qualities to the
-        // graphics card, where they will tweak the Phong lighting formula.
-        gl.uniform4fv(gpu.shape_color, material.color);
-        gl.uniform1f(gpu.ambient, material.ambient);
-        gl.uniform1f(gpu.diffusivity, material.diffusivity);
-        gl.uniform1f(gpu.specularity, material.specularity);
-        gl.uniform1f(gpu.smoothness, material.smoothness);
-    }
-
-    send_gpu_state(gl, gpu, gpu_state, model_transform) {
-        // send_gpu_state():  Send the state of our whole drawing context to the GPU.
-        const O = vec4(0, 0, 0, 1), camera_center = gpu_state.camera_transform.times(O).to3();
-        gl.uniform3fv(gpu.camera_center, camera_center);
-        // Use the squared scale trick from "Eric's blog" instead of inverse transpose matrix:
-        const squared_scale = model_transform.reduce(
-            (acc, r) => {
-                return acc.plus(vec4(...r).times_pairwise(r))
-            }, vec4(0, 0, 0, 0)).to3();
-        gl.uniform3fv(gpu.squared_scale, squared_scale);
-        // Send the current matrices to the shader.  Go ahead and pre-compute
-        // the products we'll need of the of the three special matrices and just
-        // cache and send those.  They will be the same throughout this draw
-        // call, and thus across each instance of the vertex shader.
-        // Transpose them since the GPU expects matrices as column-major arrays.
-        const PCM = gpu_state.projection_transform.times(gpu_state.camera_inverse).times(model_transform);
-        gl.uniformMatrix4fv(gpu.model_transform, false, Matrix.flatten_2D_to_1D(model_transform.transposed()));
-        gl.uniformMatrix4fv(gpu.projection_camera_model_transform, false, Matrix.flatten_2D_to_1D(PCM.transposed()));
-
-        // Omitting lights will show only the material color, scaled by the ambient term:
-        if (!gpu_state.lights.length)
-            return;
-
-        const light_positions_flattened = [], light_colors_flattened = [];
-        for (let i = 0; i < 4 * gpu_state.lights.length; i++) {
-            light_positions_flattened.push(gpu_state.lights[Math.floor(i / 4)].position[i % 4]);
-            light_colors_flattened.push(gpu_state.lights[Math.floor(i / 4)].color[i % 4]);
-        }
-        gl.uniform4fv(gpu.light_positions_or_vectors, light_positions_flattened);
-        gl.uniform4fv(gpu.light_colors, light_colors_flattened);
-        gl.uniform1fv(gpu.light_attenuation_factors, gpu_state.lights.map(l => l.attenuation));
-    }
-
-    update_GPU(context, gpu_addresses, gpu_state, model_transform, material) {
-        // update_GPU(): Define how to synchronize our JavaScript's variables to the GPU's.  This is where the shader
-        // recieves ALL of its inputs.  Every value the GPU wants is divided into two categories:  Values that belong
-        // to individual objects being drawn (which we call "Material") and values belonging to the whole scene or
-        // program (which we call the "Program_State").  Send both a material and a program state to the shaders
-        // within this function, one data field at a time, to fully initialize the shader for a draw.
-
-        // Fill in any missing fields in the Material object with custom defaults for this shader:
-        const defaults = {color: color(0, 0, 0, 1), ambient: 0, diffusivity: 1, specularity: 1, smoothness: 40};
-        material = Object.assign({}, defaults, material);
-
-        this.send_material(context, gpu_addresses, material);
-        this.send_gpu_state(context, gpu_addresses, gpu_state, model_transform);
     }
 }
 
 
 class Texture_Scroll_X extends Textured_Phong {
-    // TODO:  Modify the shader below (right now it's just the same fragment shader as Textured_Phong) for requirement #6.
     fragment_glsl_code() {
         return this.shared_glsl_code() + `
             varying vec2 f_tex_coord;
@@ -712,8 +618,16 @@ class Texture_Scroll_X extends Textured_Phong {
             uniform float animation_time;
             
             void main(){
-                // Sample the texture image in the correct place:
-                vec4 tex_color = texture2D( texture, f_tex_coord);
+                float slide_constant = 0.25*mod(animation_time, 4.0);
+                mat4 slide = mat4(
+                    vec4(1.0, 0.0, 0.0, 0.0),
+                    vec4(0.0, -1.0, 0.0, 0.0),
+                    vec4(0.0, 0.0, 1.0, 0.0),
+                    vec4(0.0, slide_constant, 0.0, 1.0)
+                );
+                vec4 new_tex_coord = slide * (vec4(f_tex_coord, 0, 0) + vec4(1.0, 1.0, 0.0, 1.0));
+                vec4 tex_color = texture2D( texture, new_tex_coord.xy);
+
                 if( tex_color.w < .01 ) discard;
                                                                          // Compute an initial (ambient) color:
                 gl_FragColor = vec4( ( tex_color.xyz + shape_color.xyz ) * ambient, shape_color.w * tex_color.w ); 
@@ -723,17 +637,24 @@ class Texture_Scroll_X extends Textured_Phong {
     }
 }
 
-
-class Texture_Rotate extends Textured_Phong {
-    // TODO:  Modify the shader below (right now it's just the same fragment shader as Textured_Phong) for requirement #7.
+class Texture_Scroll_Y extends Textured_Phong {
     fragment_glsl_code() {
         return this.shared_glsl_code() + `
             varying vec2 f_tex_coord;
             uniform sampler2D texture;
             uniform float animation_time;
+            
             void main(){
-                // Sample the texture image in the correct place:
-                vec4 tex_color = texture2D( texture, f_tex_coord );
+                float slide_constant = 0.01*mod(animation_time, 100.0);
+                mat4 slide = mat4(
+                    vec4(1.0, 0.0, 0.0, 0.0),
+                    vec4(0.0, 1.0, 0.0, 0.0),
+                    vec4(0.0, 0.0, 1.0, 0.0),
+                    vec4(slide_constant, 0.0, 0.0, 1.0)
+                );
+                vec4 new_tex_coord = slide * (vec4(f_tex_coord, 0, 0) + vec4(1.0, 1.0, 0.0, 1.0));
+                vec4 tex_color = texture2D( texture, new_tex_coord.xy);
+
                 if( tex_color.w < .01 ) discard;
                                                                          // Compute an initial (ambient) color:
                 gl_FragColor = vec4( ( tex_color.xyz + shape_color.xyz ) * ambient, shape_color.w * tex_color.w ); 
@@ -742,4 +663,3 @@ class Texture_Rotate extends Textured_Phong {
         } `;
     }
 }
-
